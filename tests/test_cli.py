0458871c8fdf1762b9aa854fb7f2f5e315c59487
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-14 15:28:08 -0700

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,4 +4,8 @@ import os
 
 
 def tests_transform_xlsx_2_csv():
+    output_path = "esfuerzos_k9.csv"
+    if os.path.exists(output_path):
+        os.remove(output_path)
     transform_xlsx_2_csv()
+    assert os.path.exists(output_path)
