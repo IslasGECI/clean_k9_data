265da486eb02781e9284835c5304e9f54cc71ba5
Mario Villasante <mario.villasante@islas.org.mx>
2023-06-14 16:25:35 -0700

    ✍🏽☦️ Add cli command

--- clean_k9_data/cli.py
+++ clean_k9_data/cli.py
@@ -1,2 +1,15 @@
-def transform_xlsx_2_csv():
-    pass
+import os
+import typer
+
+
+app = typer.Typer(
+    help="Tools to clean k9 data for the eradication Guadalupe Island project"
+)
+
+@app.command()
+def transform_xlsx():
+    """
+    Transform data `IG_ESFUERZO_K9_{date}.xls[x]` \n
+    """
+    command = "docker run -v $PWD:/workdir islasgeci/clean_k9 make transform_xlsx_2_csv"
+    os.system(command)
