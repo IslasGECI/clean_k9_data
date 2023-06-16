82154c9a7ee8b56155029536861eeb0d84507d15
Mario Villasante <mario.villasante@islas.org.mx>
2023-06-16 14:25:21 -0700

    ✅🧪 Add app commands

--- spec/transform_xlsx_to_csv_spec.sh
+++ spec/transform_xlsx_to_csv_spec.sh
@@ -8,7 +8,26 @@ Describe 'Map of active and inactivated traps'
     It 'generates file'
       When run make transform_xlsx_to_csv
       The stdout should include 'python src/transform_xlsx_to_csv.py'
+      The stderr should include 'packages/agate/utils.py:285: UnnamedColumnWarning: Column 1'
       The file /workdir/esfuerzo_k9.csv should be exist
+    End
+    It 'With explicit file name'
+      When run python src/transform_xlsx_to_csv.py esfuerzo --file=tests/data/IG_ESFUERZO_K9_03JUL2022.xlsx --hoja="Esfuerzo"
+      The stderr should include 'packages/agate/utils.py:285: UnnamedColumnWarning: Column 1'
+      The file /workdir/esfuerzo_k9.csv should be exist
+    End
+    It 'With explicit file name: check hash'
+      When call md5sum /workdir/esfuerzo_k9.csv 
+      The stdout should not include '3ee5f608acb5af6cd'
+    End
+    It 'The sheet Marcajes'
+      When run python src/transform_xlsx_to_csv.py marcaje --hoja="Marcajes "
+      The stderr should include 'packages/agate/utils.py:285: UnnamedColumnWarning: Column 1'
+      The file /workdir/marcajes_k9.csv should be exist
+    End
+    It 'Marcajes: check columns'
+      When call head /workdir/marcajes_k9.csv -n 1 
+      The stdout should include 'Fecha,Manejador,Nombre_k9,ID-punto,Coordenada_Este,Coordenada_Norte,Altitud,Zona,Tipo_de_rastro,Clave muestra,Sitio_o_lugar,Observaciones'
     End
   End
 End
--- src/transform_xlsx_to_csv.py
+++ src/transform_xlsx_to_csv.py
@@ -1,6 +1,32 @@
 import os
+import typer
 
-command = "in2csv --sheet 'Esfuerzo ' $(ls IG_ESFUERZO*) > aux.csv"
-os.system(command)
-command = "csvcut -c '1-9' -K 3 aux.csv > esfuezo_k9.csv"
-os.system(command)
+
+def extract_sheets(hoja: str = "Esfuerzo ", file: str = "tests/data/IG_ESFUERZO_28MAY2023.xls"):
+    command = f"in2csv --sheet '{hoja}' {file} > aux.csv"
+    os.system(command)
+
+
+app = typer.Typer(help="Tools to clean k9 data for the eradication Guadalupe Island project")
+
+
+@app.command()
+def esfuerzo(hoja: str = "Esfuerzo ", file: str = "tests/data/IG_ESFUERZO_28MAY2023.xls"):
+    extract_sheets(hoja, file)
+    command = "csvcut -c '1-9' -K 3 aux.csv > esfuerzo_k9.csv"
+    os.system(command)
+
+
+@app.command()
+def marcaje(hoja: str = "Marcajes ", file: str = "tests/data/IG_ESFUERZO_28MAY2023.xls"):
+    extract_sheets(hoja, file)
+    command = "csvcut -c '1-10,12,13' -K 3 aux.csv > marcajes_k9.csv"
+    os.system(command)
+
+
+@app.command()
+def version():
+    print("hola")
+
+
+app()
