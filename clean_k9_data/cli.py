281a52f024cc0d1b97bce921055d94e8c8001436
Mario Villasante <mario.villasante@islas.org.mx>
2023-06-20 11:55:18 -0700

    🚚 Move functions to module

--- clean_k9_data/cli.py
+++ clean_k9_data/cli.py
@@ -2,13 +2,28 @@ import os
 import typer
 
 
+def extract_sheets(hoja: str = "Esfuerzo ", file: str = "tests/data/IG_ESFUERZO_28MAY2023.xls"):
+    command = f"in2csv --sheet '{hoja}' {file} > aux.csv"
+    os.system(command)
+
+
 app = typer.Typer(help="Tools to clean k9 data for the eradication Guadalupe Island project")
 
 
 @app.command()
-def transform_xlsx():
-    """
-    Transform data `IG_ESFUERZO_K9_{date}.xls[x]` \n
-    """
-    command = "docker run -v $PWD:/workdir islasgeci/clean_k9 make transform_xlsx_2_csv"
+def esfuerzo(hoja: str = "Esfuerzo ", file: str = "tests/data/IG_ESFUERZO_28MAY2023.xls"):
+    extract_sheets(hoja, file)
+    command = "csvcut -c '1-9' -K 3 aux.csv > esfuerzo_k9.csv"
     os.system(command)
+
+
+@app.command()
+def marcaje(hoja: str = "Marcajes ", file: str = "tests/data/IG_ESFUERZO_28MAY2023.xls"):
+    extract_sheets(hoja, file)
+    command = "csvcut -c '1-10,12,13' -K 3 aux.csv > marcajes_k9.csv"
+    os.system(command)
+
+
+@app.command()
+def version():
+    print("hola")
